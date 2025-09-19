48be275670a977f0d42a4edf2ace441414aa6828
ezekielskeene <113312161+ezekielskeene@users.noreply.github.com>
2025-09-18 20:34:16 -0400

    small fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/sampleMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/sampleMecanumTeleOp.java
@@ -4,6 +4,11 @@ import static org.firstinspires.ftc.teamcode.support.Constants.pinpointXOffset;
 import static org.firstinspires.ftc.teamcode.support.Constants.pinpointYOffset;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -12,11 +17,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.support.PinpointOdo;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
 @TeleOp
 public class sampleMecanumTeleOp extends CommandOpMode {
 
     public DcMotorEx leftFront, rightFront, leftBack, rightBack;
     PinpointOdo odo;
+    private Limelight3A limelight;
+    public static double HEADING_KP_TX = 0.023;
+    public static double ROTATION_MIN_POWER = 0.0;
+    private boolean isHeadingLocked = true;
+    double finalRotation;
+    GamepadEx driver;
 
     @Override
     public void initialize() {
@@ -24,15 +38,30 @@ public class sampleMecanumTeleOp extends CommandOpMode {
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
         rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        driver = new GamepadEx(gamepad1);
 
-        odo = hardwareMap.get(PinpointOdo.class,"odo");
-        odo.setOffsets(pinpointXOffset,pinpointYOffset);
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+        limelight.start();
+
+        odo = hardwareMap.get(PinpointOdo.class, "odo");
+        odo.setOffsets(pinpointXOffset, pinpointYOffset);
         odo.setEncoderResolution(PinpointOdo.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         odo.setEncoderDirections(PinpointOdo.EncoderDirection.REVERSED,
                 PinpointOdo.EncoderDirection.FORWARD);
         odo.resetPosAndIMU();
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_UP)
+                .whenPressed(new InstantCommand(() -> {
+                    isHeadingLocked = true;
+                }));
+        driver.getGamepadButton(GamepadKeys.Button.DPAD_DOWN)
+                .whenPressed(new InstantCommand(() -> {
+                    isHeadingLocked = false;
+                }));
 
     }
 
@@ -46,6 +75,29 @@ public class sampleMecanumTeleOp extends CommandOpMode {
         double x = gamepad1.left_stick_x;
         double y = gamepad1.left_stick_y;
         double rx = gamepad1.right_stick_x;
+
+        LLResult result = limelight.getLatestResult();
+
+
+
+        if (isHeadingLocked) {
+
+
+            double error = result.getTxNC();
+
+
+            if (result.isValid()) {
+                //finalRotation = (error * HEADING_KP_TX) + (integralSum * HEADING_KI_TX) + (derivative * HEADING_KD_TX);
+                finalRotation = (error * HEADING_KP_TX);
+
+                if (Math.abs(finalRotation) > 0 && Math.abs(finalRotation) < ROTATION_MIN_POWER) {
+                    rx = Math.signum(finalRotation) * ROTATION_MIN_POWER;
+                }
+            } else {
+                rx = 0;
+            }
+
+        }
 
         double rotX = x * Math.cos(-botHeading) + y * Math.sin(-botHeading);
         double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
@@ -61,15 +113,16 @@ public class sampleMecanumTeleOp extends CommandOpMode {
         double backRightPower = (rotY + rotX - rx) / denominator;
 
 
+
+
         // Set the motor powers
         leftFront.setPower(frontLeftPower);
         leftBack.setPower(backLeftPower);
         rightFront.setPower(frontRightPower);
         rightBack.setPower(backRightPower);
 
-        if(gamepad1.right_stick_button) {
+        if (gamepad1.right_stick_button) {
             odo.resetPosAndIMU();
         }
-
     }
 }
