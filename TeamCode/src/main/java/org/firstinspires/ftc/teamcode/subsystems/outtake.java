3d150d5cccf2b591a8c7c2849f8c0a0f97aa0376
tanushri1717 <tanushri.iyyadurai@circuitrunners.com>
2025-10-10 16:58:45 -0400

    Finished subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/outtake.java
@@ -9,15 +9,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class outtake extends SubsystemBase {
     public DcMotorEx continiousOuttake;
     public Servo blockingServo;
+    public Servo blockingServoTwo;
     public Servo aimingServo;
 
     public double blockingPosition = 0;
+    public double blockingPositionTwo = 0;
     public double nonBlockingPosition=1;
+    public double nonBlockingPositionTwo=1;
 
 
     public outtake(HardwareMap hardwareMap) {
         continiousOuttake = hardwareMap.get(DcMotorEx.class, "outtake");
-        blockingServo= hardwareMap.get(Servo.class, "blockingServo");
+        blockingServo= hardwareMap.get(Servo.class, "blockingServoRight");
+        blockingServoTwo= hardwareMap.get(Servo.class, "blockingServoLeft");
         aimingServo = hardwareMap.get(Servo.class, "launchingServo");
 
 }
@@ -29,14 +33,25 @@ public class outtake extends SubsystemBase {
         }
     public void setBlock () {
         blockingServo.setPosition(blockingPosition);
+        blockingServoTwo.setPosition(blockingPositionTwo);
 
     }
-    public void nonBlock() {
+    public void nonBlock(){
         blockingServo.setPosition(nonBlockingPosition);
-    }
-    public void aiming (boolean isUp, boolean isDown){
-        double currentPosition = aimingServo.getPosition();
+        blockingServoTwo.setPosition(nonBlockingPositionTwo);
     }
 
+    public void aiming ( boolean isUp, boolean isDown)
+        {double currentPosition = aimingServo.getPosition();
+            aimingServo.setPosition(currentPosition+0.11);
+            if (isUp){
+                aimingServo.setPosition(currentPosition+0.11);
 
-    }
+            }
+                else if (isDown){
+                    aimingServo.setPosition(currentPosition-0.11);
+
+            }
+
+        }
+}
