377759d6d1a569502c10f1981732f47e662de850
ConcaveEgg <75274393+ConcaveEgg@users.noreply.github.com>
2025-10-09 22:52:40 -0400

    updating stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
@@ -1,25 +1,23 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class intake extends SubsystemBase {
-        public DcMotorEx intakeRoller;
-        public DcMotorEx transferMech;
+    public DcMotorEx intakeRoller;
+    public DcMotorEx transferMech;
 
+    public intake(HardwareMap hardwareMap) {
+        intakeRoller = hardwareMap.get(DcMotorEx.class, "intake");
+        transferMech = hardwareMap.get(DcMotorEx.class, "transfer");
+    }
 
-
-        public intake(HardwareMap hardwareMap) {
-            intakeRoller = hardwareMap.get(DcMotorEx.class, "intake");
-            transferMech = hardwareMap.get(DcMotorEx.class, "transfer");
-
-            }
     public void runIntake(double forward) {
         intakeRoller.setPower(forward);
     }
+
     public void setTransferMech(double power) {
             transferMech.setPower(power);
     }
-        }
+}
