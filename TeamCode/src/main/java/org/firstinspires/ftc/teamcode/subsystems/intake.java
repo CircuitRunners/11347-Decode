829b14248ccb253c8503de65b8f97b062c680f88
tanushri1717 <tanushri.iyyadurai@circuitrunners.com>
2025-10-10 17:27:39 -0400

    Finished subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
@@ -1,25 +1,40 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@Config
 public class intake extends SubsystemBase {
-        public DcMotorEx intakeRoller;
-        public DcMotorEx transferMech;
+    public DcMotorEx intakeRoller;
+    public DcMotorEx transferMech;
 
+    public static boolean intakeDirection = true;
+    public static boolean transferDirection = true;
 
+    public intake(HardwareMap hardwareMap) {
+        intakeRoller = hardwareMap.get(DcMotorEx.class, "intake");
+        transferMech = hardwareMap.get(DcMotorEx.class, "transfer");
 
-        public intake(HardwareMap hardwareMap) {
-            intakeRoller = hardwareMap.get(DcMotorEx.class, "intake");
-            transferMech = hardwareMap.get(DcMotorEx.class, "transfer");
+    }
 
-            }
     public void runIntake(double forward) {
-        intakeRoller.setPower(forward);
-    }
-    public void setTransferMech(double power) {
-            transferMech.setPower(power);
-    }
+        double power = forward;
+        if (!intakeDirection){
+            power = -forward;
         }
+
+        intakeRoller.setPower(power);
+    }
+
+    public void setTransferMech(double forward) {
+        double power = forward;
+        if(!transferDirection){
+            power = - forward;
+        }
+
+        transferMech.setPower(power);
+    }
+}
