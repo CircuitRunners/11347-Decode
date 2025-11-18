f49d89f2830417faea6a0dc17db57eabfcc5adff
ezekielskeene <113312161+ezekielskeene@users.noreply.github.com>
2025-11-18 17:07:15 -0500

    fully functional but very basic TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/outreach/TankDrive.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.teleOp.outreach;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TankDrive extends SubsystemBase {
+    public DcMotorEx leftMotor1;
+    public DcMotorEx leftMotor2;
+
+    public DcMotorEx rightMotor1;
+    public DcMotorEx rightMotor2;
+
+    public void init(HardwareMap hardwareMap) {
+        leftMotor1 = hardwareMap.get(DcMotorEx.class, "lm");
+        rightMotor1 = hardwareMap.get(DcMotorEx.class, "rm");
+
+        leftMotor2 = hardwareMap.get(DcMotorEx.class, "lm2");
+        rightMotor2 = hardwareMap.get(DcMotorEx.class, "lm2");
+
+        DcMotorEx[] motors = new DcMotorEx[]{
+                leftMotor1,rightMotor2,leftMotor2,rightMotor2
+
+};
